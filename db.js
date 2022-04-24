cebc5f7dcec7928df0bb572f6a0b387166fa24a0
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-04-24 12:31:42 +0200

    answer and questions

--- db.js
+++ db.js
@@ -14,60 +14,61 @@ let questions = [{
         "answer4": "16",
         "rightAnswer": 3
     },
-    {
-        "question": "Wie viele Runden gibt es im Profiboxen höchstens?",
-        "answer1": "10",
-        "answer2": "13",
-        "answer3": "14",
-        "answer4": "12",
-        "rightAnswer": 4
-    },
-    {
-        "question": "Wie nennt man die dritthöchste Liga im deutschen Profifußball?",
-        "answer1": "Regionalliga",
-        "answer2": "Verbandsliga",
-        "answer3": "3. Liga",
-        "answer4": "Landesliga",
-        "rightAnswer": 3
-    },
-    {
-        "question": "Was bedeutet die Abkürzung DHB?",
-        "answer1": "Deutscher Handballbund",
-        "answer2": "Deutscher Hochsprungbund",
-        "answer3": "Deutscher Schwersport",
-        "answer4": "Deutscher Hockeybund",
-        "rightAnswer": 1
-    }, {
-        "question": " Welche Nation gewann bei den Olympischen Spielen 2012 in London die meisten Goldmedaillen?",
-        "answer1": "Deutschland",
-        "answer2": "USA",
-        "answer3": "Russland",
-        "answer4": "China",
-        "rightAnswer": 2
-    },
-    {
-        "question": ": Wofür steht die Abkürzung UEFA?",
-        "answer1": "United European Football Alliance",
-        "answer2": "Union of European Football Associations",
-        "answer3": "United European Football Associations",
-        "answer4": "Union of European Football Alliance",
-        "rightAnswer": 2
-    },
-    {
-        "question": "Wer entwickelte die Rechenmaschine Analytical Engine",
-        "answer1": "Charles Babbage",
-        "answer2": "Konrad Zuse",
-        "answer3": "Nina Riedelbauch",
-        "answer4": "Cedric Rahmel",
-        "rightAnswer": 1
-    },
-    {
-        "question": "wie heißt die programmiersprache des internets",
-        "answer1": "C#",
-        "answer2": "C++",
-        "answer3": "Java",
-        "answer4": "JavaScript",
-        "rightAnswer": 4
-    }
+    /*
+        {
+            "question": "Wie viele Runden gibt es im Profiboxen höchstens?",
+            "answer1": "10",
+            "answer2": "13",
+            "answer3": "14",
+            "answer4": "12",
+            "rightAnswer": 4
+        },
+        {
+            "question": "Wie nennt man die dritthöchste Liga im deutschen Profifußball?",
+            "answer1": "Regionalliga",
+            "answer2": "Verbandsliga",
+            "answer3": "3. Liga",
+            "answer4": "Landesliga",
+            "rightAnswer": 3
+        },
+        {
+            "question": "Was bedeutet die Abkürzung DHB?",
+            "answer1": "Deutscher Handballbund",
+            "answer2": "Deutscher Hochsprungbund",
+            "answer3": "Deutscher Schwersport",
+            "answer4": "Deutscher Hockeybund",
+            "rightAnswer": 1
+        }, {
+            "question": " Welche Nation gewann bei den Olympischen Spielen 2012 in London die meisten Goldmedaillen?",
+            "answer1": "Deutschland",
+            "answer2": "USA",
+            "answer3": "Russland",
+            "answer4": "China",
+            "rightAnswer": 2
+        },
+        {
+            "question": ": Wofür steht die Abkürzung UEFA?",
+            "answer1": "United European Football Alliance",
+            "answer2": "Union of European Football Associations",
+            "answer3": "United European Football Associations",
+            "answer4": "Union of European Football Alliance",
+            "rightAnswer": 2
+        },
+        {
+            "question": "Wer entwickelte die Rechenmaschine Analytical Engine",
+            "answer1": "Charles Babbage",
+            "answer2": "Konrad Zuse",
+            "answer3": "Nina Riedelbauch",
+            "answer4": "Cedric Rahmel",
+            "rightAnswer": 1
+        },
+        {
+            "question": "wie heißt die programmiersprache des internets",
+            "answer1": "C#",
+            "answer2": "C++",
+            "answer3": "Java",
+            "answer4": "JavaScript",
+            "rightAnswer": 4
+        }*/
 ];
 let currentQuestion = 0;
--- script.js
+++ script.js
@@ -1,17 +1,60 @@
 function init() {
     document.getElementById('allQuestions').innerHTML = questions.length;
+    //Hier zeige ich die anzhal der Fragen an mit questions.length
     showQuestion();
 }
 
+
 function showQuestion() {
-    let question = questions[currentQuestion];
-    document.getElementById('questionText').innerHTML = question['question'];
-    document.getElementById('answer1').innerHTML = question['answer1'];
-    document.getElementById('answer2').innerHTML = question['answer2'];
-    document.getElementById('answer3').innerHTML = question['answer3'];
-    document.getElementById('answer4').innerHTML = question['answer4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = '';
+        document.getElementById('quizBody').style = 'display: none;';
+        document.getElementById('questionMarkPicture').style = 'display: none;';
+    } else {
+        let question = questions[currentQuestion];
+        document.getElementById('questionNumber').innerHTML = currentQuestion + 1;
+        //Hier zeige ich die Aktuelle Frage an als z.B 2 von 7
+        document.getElementById('questionText').innerHTML = question['question'];
+        document.getElementById('answer1').innerHTML = question['answer1'];
+        document.getElementById('answer2').innerHTML = question['answer2'];
+        document.getElementById('answer3').innerHTML = question['answer3'];
+        document.getElementById('answer4').innerHTML = question['answer4'];
+    }
 }
 
-function answer(selection) {
 
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    // mit .slice(-1) hole ich mir das letze Zeichen von einem String
+    let idOfRightAnswer = `answer${question['rightAnswer']}`;
+
+    if (selectedQuestionNumber == question['rightAnswer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        // mit .parentNode hole ich mir das Übergeordnete HTML Element
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('nextButton').disabled = false;
+}
+
+
+function nextQuestion() {
+    currentQuestion++; // Wird von 0 auf z.B 1 erhöht 
+    showQuestion();
+    document.getElementById('nextButton').disabled = true;
+    resetAnswers();
+}
+
+
+function resetAnswers() {
+    document.getElementById('answer1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer4').parentNode.classList.remove('bg-success');
 }
